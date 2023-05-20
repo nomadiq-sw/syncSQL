55794a7b9a4b23e2445ad0e3065fe1fd0ac33f38
tok3rat0r <owenmjones1@gmail.com>
2023-05-20 18:47:15 +0200

    Setup connections and ping databases

--- cmd/dbconn/dbconn.go
+++ cmd/dbconn/dbconn.go
@@ -1,5 +1,49 @@
 package dbconn
 
-func SayHello() string {
-	return "Hello from dbconn!"
+import (
+	"database/sql"
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/go-sql-driver/mysql"
+	_ "github.com/jackc/pgx/v5/stdlib"
+)
+
+func connect_db(driver string, url string) {
+	db, err := sql.Open(driver, url)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to connect to %v database: %v\n", driver, err)
+		os.Exit(1)
+	}
+
+	fmt.Fprintf(os.Stderr, "Successfully connected to %v database!\n", driver)
+	db.SetConnMaxLifetime(time.Minute * 3)
+	db.SetMaxOpenConns(1)
+	db.SetMaxIdleConns(1)
+
+	err = db.Ping()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to ping %v database: %v\n", driver, err)
+		os.Exit(1)
+	}
+
+	fmt.Fprintf(os.Stderr, "Successfully pinged %v database!\n", driver)
+}
+
+func Connect_PGSQL(url string) {
+	connect_db("pgx", url)
+}
+
+func Connect_MySQL(user string, password string, db_name string, host_port string) {
+	cfg := mysql.Config{
+		User: user,
+		Passwd: password,
+		Net: "tcp",
+		DBName: db_name,
+		Addr: host_port,
+		AllowNativePasswords: true,
+	}
+
+	connect_db("mysql", cfg.FormatDSN())
 }
--- main.go
+++ main.go
@@ -1,9 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"os"
 
-import "github.com/nomadiq-sw/syncSQL/cmd/dbconn"
+	"github.com/nomadiq-sw/syncSQL/cmd/dbconn"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println(dbconn.SayHello())
+	godotenv.Load()
+
+	dbconn.Connect_PGSQL(os.Getenv("POSTGRESQL_URL"))
+	dbconn.Connect_MySQL(
+		os.Getenv("MYSQL_USER"),
+		os.Getenv("MYSQL_PASS"),
+		os.Getenv("MYSQL_DB_NAME"),
+		os.Getenv("MYSQL_HOST_PORT"),
+	)
 }
