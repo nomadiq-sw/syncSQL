7d241520cd0e835be3542b788eff51b54e428c70
tok3rat0r <owenmjones1@gmail.com>
2023-05-21 13:46:03 +0200

    Use goroutines and channels for concurrent connections

--- cmd/dbconn/dbconn.go
+++ cmd/dbconn/dbconn.go
@@ -2,40 +2,42 @@ package dbconn
 
 import (
 	"database/sql"
-	"fmt"
-	"os"
+	"log"
 	"time"
 
 	"github.com/go-sql-driver/mysql"
 	_ "github.com/jackc/pgx/v5/stdlib"
 )
 
-func connect_db(driver string, url string) {
+func connect_db(driver string, url string) *sql.DB {
 	db, err := sql.Open(driver, url)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Unable to connect to %v database: %v\n", driver, err)
-		os.Exit(1)
+		log.Fatalf("Unable to connect to %v database: %v\n", driver, err)
 	}
 
-	fmt.Fprintf(os.Stderr, "Successfully connected to %v database!\n", driver)
+	log.Printf("Successfully connected to %v database\n", driver)
 	db.SetConnMaxLifetime(time.Minute * 3)
 	db.SetMaxOpenConns(1)
 	db.SetMaxIdleConns(1)
 
 	err = db.Ping()
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Unable to ping %v database: %v\n", driver, err)
-		os.Exit(1)
+		log.Fatalf("Unable to ping %v database: %v\n", driver, err)
 	}
 
-	fmt.Fprintf(os.Stderr, "Successfully pinged %v database!\n", driver)
+	log.Printf("Successfully pinged %v database\n", driver)
+
+	return db
 }
 
-func Connect_PGSQL(url string) {
-	connect_db("pgx", url)
+func Connect_PGSQL(url string, connect chan *sql.DB) {
+	pg := connect_db("pgx", url)
+	if connect != nil {
+		connect <- pg
+	}
 }
 
-func Connect_MySQL(user string, password string, db_name string, host_port string) {
+func Connect_MySQL(user string, password string, db_name string, host_port string, connect chan *sql.DB) {
 	cfg := mysql.Config{
 		User: user,
 		Passwd: password,
@@ -45,5 +47,8 @@ func Connect_MySQL(user string, password string, db_name string, host_port strin
 		AllowNativePasswords: true,
 	}
 
-	connect_db("mysql", cfg.FormatDSN())
+	my := connect_db("mysql", cfg.FormatDSN())
+	if connect != nil {
+		connect <- my
+	}
 }
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"database/sql"
 	"os"
 
 	"github.com/nomadiq-sw/syncSQL/cmd/dbconn"
@@ -11,11 +12,17 @@ import (
 func main() {
 	godotenv.Load()
 
-	dbconn.Connect_PGSQL(os.Getenv("POSTGRESQL_URL"))
-	dbconn.Connect_MySQL(
+	pg_chan := make(chan *sql.DB)
+	my_chan := make(chan *sql.DB)
+
+	go dbconn.Connect_PGSQL(os.Getenv("POSTGRESQL_URL"), pg_chan)
+	go dbconn.Connect_MySQL(
 		os.Getenv("MYSQL_USER"),
 		os.Getenv("MYSQL_PASS"),
 		os.Getenv("MYSQL_DB_NAME"),
 		os.Getenv("MYSQL_HOST_PORT"),
+		my_chan,
 	)
+	<- pg_chan
+	<- my_chan
 }
